1cead1bd97b8c58076d2b04dfc544bed7a556ed1
fredericoamsb <fredericoamsb@hotmail.com>
2019-11-26 05:59:56 -0300

    Buttons, add and remove lists #2

--- src/App.vue
+++ src/App.vue
@@ -16,14 +16,32 @@
             <div class="card">
               <div class="card-body">
                 <ul class="list-group">
-                  <li
-                    class="list-group-item"
-                    v-for="prefix in prefixes"
-                    v-bind:key="prefix"
-                  >{{prefix}}</li>
+                  <li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
+                    <div class="row">
+                      <div class="col-md">{{prefix}}</div>
+                      <div class="col-md text-right">
+                        <button class="btn btn-info" v-on:click="deletePrefix(prefix)">
+                          <span class="fa fa-trash"></span>
+                        </button>
+                      </div>
+                    </div>
+                  </li>
                 </ul>
                 <br />
-                <input class="form-control" type="text" placeholder="Digite o prefixo" />
+                <div class="input-group">
+                  <input
+                    class="form-control"
+                    type="text"
+                    v-model="prefix"
+                    v-on:keyup.enter="addPrefix(prefix)"
+                    placeholder="Digite o prefixo"
+                  />
+                  <div class="input-group-append">
+                    <button class="btn btn-info" v-on:click="addPrefix(prefix)">
+                      <span class="fa fa-plus"></span>
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -35,20 +53,43 @@
             <div class="card">
               <div class="card-body">
                 <ul class="list-group">
-                  <li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">{{sufix}}</li>
+                  <li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
+                    <div class="row">
+                      <div class="col-md">{{sufix}}</div>
+                      <div class="col-md text-right">
+                        <button class="btn btn-info" v-on:click="deleteSufix(sufix)">
+                          <span class="fa fa-trash"></span>
+                        </button>
+                      </div>
+                    </div>
+                  </li>
                 </ul>
                 <br />
-                <input class="form-control" type="text" placeholder="Digite o sufixo" />
+                <div class="input-group">
+                  <input
+                    class="form-control"
+                    type="text"
+                    v-model="sufix"
+                    v-on:keyup.enter="addSufix(sufix)"
+                    placeholder="Digite o sufixo"
+                  />
+                  <div class="input-group-append">
+                    <button class="btn btn-info" v-on:click="addSufix(sufix)">
+                      <span class="fa fa-plus"></span>
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <br />
+        <h5>
+          Domínios
+          <span class="badge badge-info">{{domains.length}}</span>
+        </h5>
         <div class="card">
           <div class="card-body">
-            <h5>
-              Domínios
-              <span class="badge badge-info">{{domains.length}}</span>
-            </h5>
             <ul class="list-group">
               <li class="list-group-item" v-for="domain in domains" v-bind:key="domain">{{domain}}</li>
             </ul>
@@ -65,8 +106,10 @@ import "font-awesome/css/font-awesome.css";
 
 export default {
   name: "app",
-  data: function() {
+  data: () => {
     return {
+      prefix: "",
+      sufix: "",
       prefixes: ["Air", "Jet", "Flight"],
       sufixes: ["Hub", "Station", "Mart"],
       domains: [
@@ -81,6 +124,34 @@ export default {
         "FlightMart"
       ]
     };
+  },
+  methods: {
+    addPrefix(prefix) {
+      this.prefixes.push(prefix);
+      this.prefix = "";
+      this.generate();
+    },
+    deletePrefix(prefix) {
+      this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
+      this.generate();
+    },
+    addSufix(sufix) {
+      this.sufixes.push(sufix);
+      this.sufix = "";
+      this.generate();
+    },
+    deleteSufix(sufix) {
+      this.sufixes.splice(this.sufixes.indexOf(sufix), 1);
+      this.generate();
+    },
+    generate() {
+      this.domains = [];
+      for (const prefix of this.prefixes) {
+        for (const sufix of this.sufixes) {
+          this.domains.push(prefix + sufix);
+        }
+      }
+    }
   }
 };
 </script>
